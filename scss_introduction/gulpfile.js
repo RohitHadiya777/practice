98bbb1c1f372d4a72bb23e0a194127f291cd024d
RohitHadiya777 <rohithadiya.20.imsc@iict.indusuni.ac.in>
2023-08-13 16:34:17 +0530

    sass partials and import

--- scss_introduction/gulpfile.js
+++ scss_introduction/gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function createStyle() {
-    return src('index.scss')
+    return src('*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchStyle() {
-    watch(['index.scss'], createStyle)
+    watch(['*.scss'], createStyle)
 }
 
 exports.default = series(createStyle, watchStyle)
